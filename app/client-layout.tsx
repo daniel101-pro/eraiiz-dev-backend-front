6fdf159aceea0d7c1b83ef5f30631c2ede90e60d
Daniel Falodun <falodun379@gmail.com>
2025-06-11 23:24:02 -0700

    big updates

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -6,8 +6,8 @@ import { CurrencyProvider } from './context/CurrencyContext';
 import { Toaster } from 'react-hot-toast';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-// Get the Google Client ID from environment variables with a default value for development
-const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '1034484574619-vc3gv4qe3tkv0vfcg2iqldj5ju3e3s7u.apps.googleusercontent.com';
+// Get the Google Client ID from environment variables
+const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '429710625418-18g91vccienk7nhsvchdeehctl9dsl.apps.googleusercontent.com';
 
 export default function ClientLayout({
   children,
